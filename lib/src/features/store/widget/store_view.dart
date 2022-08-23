caf4703677afd14de49c322a162702ea89d26115
Tembeon <tembeon@gmail.com>
2022-08-23 15:32:00 +0300

    use CustomScrollView for render StoreView body

--- lib/src/features/store/widget/store_view.dart
+++ lib/src/features/store/widget/store_view.dart
@@ -7,6 +7,12 @@ import 'hot_sales_store.dart';
 import 'search_bar.dart';
 import 'top_bar.dart';
 
+/// Storing [StoreItems] in context.
+///
+/// You can get data via:
+/// ```
+/// MainStore.of(context).storeItems;
+/// ```
 class MainStore extends InheritedWidget {
   const MainStore({
     Key? key,
@@ -14,16 +20,22 @@ class MainStore extends InheritedWidget {
     required Widget child,
   }) : super(key: key, child: child);
 
+  /// Storing actual store data.
   final StoreItems storeItems;
 
+  /// Retrieve data via context.
+  ///
+  /// Set [listen] to [true] if you want to subscribe to widget updates.
   static MainStore of(BuildContext context, {bool listen = false}) {
     if (listen) {
+      // subscribe given [context] to updates.
       final MainStore? result =
           context.dependOnInheritedWidgetOfExactType<MainStore>();
       assert(result != null, 'No MainStore found in context');
 
       return result!;
     } else {
+      // just return actual value without subscription
       final inhWidget =
           context.getElementForInheritedWidgetOfExactType<MainStore>()?.widget;
       assert(inhWidget != null, 'No MainStore found in context');
@@ -32,11 +44,13 @@ class MainStore extends InheritedWidget {
     }
   }
 
+  // notify about updates if [storeItems] has been changed.
   @override
   bool updateShouldNotify(MainStore oldWidget) =>
       oldWidget.storeItems != storeItems;
 }
 
+/// View with store UI. You should pass [MainStore] as parent in widget tree.
 class StoreView extends StatefulWidget {
   const StoreView({
     Key? key,
@@ -52,17 +66,26 @@ class _StoreViewState extends State<StoreView> {
     return Scaffold(
       appBar: const TopBar(),
       body: SafeArea(
-        minimum: const EdgeInsets.symmetric(horizontal: 14.0),
-        child: Column(
-          children: const <Widget>[
-            CategoriesSection(),
-            SizedBox(height: 36.0),
-            SearchBar(),
-            SizedBox(
-              height: 24.0,
+        minimum: const EdgeInsets.symmetric(horizontal: 8.0),
+        bottom: false,
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+                children: const <Widget>[
+                  CategoriesSection(),
+                  SizedBox(height: 36.0),
+                  SearchBar(),
+                  SizedBox(height: 24.0),
+                  HotSalesSection(),
+                ],
+              ),
             ),
-            HotSalesSection(),
-            BestSellerSection(),
+            const SliverToBoxAdapter(child: SizedBox(height: 12)),
+            const SliverToBoxAdapter(child: BestSellerSection()),
+            const BestSalesGridView(),
+            // additional space from bottom for full visible grid items
+            const SliverToBoxAdapter(child: SizedBox(height: 100)),
           ],
         ),
       ),
