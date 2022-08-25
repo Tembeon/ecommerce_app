8d3e8df674bb54935627622bf905173e1ebce4f7
Tembeon <tembeon@gmail.com>
2022-08-25 17:24:44 +0300

    add details model

---
+++ lib/src/features/item_details/model/details.freezed.dart
@@ -0,0 +1,439 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'details.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+DetailsModel _$DetailsModelFromJson(Map<String, dynamic> json) {
+  return _DetailsModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$DetailsModel {
+  @JsonKey(name: 'CPU')
+  String get cpu => throw _privateConstructorUsedError;
+  @JsonKey(name: 'camera')
+  String get camera => throw _privateConstructorUsedError;
+  @JsonKey(name: 'capacity')
+  List<String> get capacity => throw _privateConstructorUsedError;
+  @JsonKey(name: 'color')
+  List<String> get color => throw _privateConstructorUsedError;
+  @JsonKey(name: 'id')
+  String get id => throw _privateConstructorUsedError;
+  @JsonKey(name: 'images')
+  List<String> get images => throw _privateConstructorUsedError;
+  @JsonKey(name: 'isFavorites')
+  bool get isFavorites => throw _privateConstructorUsedError;
+  @JsonKey(name: 'price')
+  int get price => throw _privateConstructorUsedError;
+  @JsonKey(name: 'rating')
+  double get rating => throw _privateConstructorUsedError;
+  @JsonKey(name: 'sd')
+  String get sd => throw _privateConstructorUsedError;
+  @JsonKey(name: 'ssd')
+  String get ssd => throw _privateConstructorUsedError;
+  @JsonKey(name: 'title')
+  String get title => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $DetailsModelCopyWith<DetailsModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $DetailsModelCopyWith<$Res> {
+  factory $DetailsModelCopyWith(
+          DetailsModel value, $Res Function(DetailsModel) then) =
+      _$DetailsModelCopyWithImpl<$Res>;
+  $Res call(
+      {@JsonKey(name: 'CPU') String cpu,
+      @JsonKey(name: 'camera') String camera,
+      @JsonKey(name: 'capacity') List<String> capacity,
+      @JsonKey(name: 'color') List<String> color,
+      @JsonKey(name: 'id') String id,
+      @JsonKey(name: 'images') List<String> images,
+      @JsonKey(name: 'isFavorites') bool isFavorites,
+      @JsonKey(name: 'price') int price,
+      @JsonKey(name: 'rating') double rating,
+      @JsonKey(name: 'sd') String sd,
+      @JsonKey(name: 'ssd') String ssd,
+      @JsonKey(name: 'title') String title});
+}
+
+/// @nodoc
+class _$DetailsModelCopyWithImpl<$Res> implements $DetailsModelCopyWith<$Res> {
+  _$DetailsModelCopyWithImpl(this._value, this._then);
+
+  final DetailsModel _value;
+  // ignore: unused_field
+  final $Res Function(DetailsModel) _then;
+
+  @override
+  $Res call({
+    Object? cpu = freezed,
+    Object? camera = freezed,
+    Object? capacity = freezed,
+    Object? color = freezed,
+    Object? id = freezed,
+    Object? images = freezed,
+    Object? isFavorites = freezed,
+    Object? price = freezed,
+    Object? rating = freezed,
+    Object? sd = freezed,
+    Object? ssd = freezed,
+    Object? title = freezed,
+  }) {
+    return _then(_value.copyWith(
+      cpu: cpu == freezed
+          ? _value.cpu
+          : cpu // ignore: cast_nullable_to_non_nullable
+              as String,
+      camera: camera == freezed
+          ? _value.camera
+          : camera // ignore: cast_nullable_to_non_nullable
+              as String,
+      capacity: capacity == freezed
+          ? _value.capacity
+          : capacity // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      color: color == freezed
+          ? _value.color
+          : color // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      images: images == freezed
+          ? _value.images
+          : images // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      isFavorites: isFavorites == freezed
+          ? _value.isFavorites
+          : isFavorites // ignore: cast_nullable_to_non_nullable
+              as bool,
+      price: price == freezed
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as int,
+      rating: rating == freezed
+          ? _value.rating
+          : rating // ignore: cast_nullable_to_non_nullable
+              as double,
+      sd: sd == freezed
+          ? _value.sd
+          : sd // ignore: cast_nullable_to_non_nullable
+              as String,
+      ssd: ssd == freezed
+          ? _value.ssd
+          : ssd // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$$_DetailsModelCopyWith<$Res>
+    implements $DetailsModelCopyWith<$Res> {
+  factory _$$_DetailsModelCopyWith(
+          _$_DetailsModel value, $Res Function(_$_DetailsModel) then) =
+      __$$_DetailsModelCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {@JsonKey(name: 'CPU') String cpu,
+      @JsonKey(name: 'camera') String camera,
+      @JsonKey(name: 'capacity') List<String> capacity,
+      @JsonKey(name: 'color') List<String> color,
+      @JsonKey(name: 'id') String id,
+      @JsonKey(name: 'images') List<String> images,
+      @JsonKey(name: 'isFavorites') bool isFavorites,
+      @JsonKey(name: 'price') int price,
+      @JsonKey(name: 'rating') double rating,
+      @JsonKey(name: 'sd') String sd,
+      @JsonKey(name: 'ssd') String ssd,
+      @JsonKey(name: 'title') String title});
+}
+
+/// @nodoc
+class __$$_DetailsModelCopyWithImpl<$Res>
+    extends _$DetailsModelCopyWithImpl<$Res>
+    implements _$$_DetailsModelCopyWith<$Res> {
+  __$$_DetailsModelCopyWithImpl(
+      _$_DetailsModel _value, $Res Function(_$_DetailsModel) _then)
+      : super(_value, (v) => _then(v as _$_DetailsModel));
+
+  @override
+  _$_DetailsModel get _value => super._value as _$_DetailsModel;
+
+  @override
+  $Res call({
+    Object? cpu = freezed,
+    Object? camera = freezed,
+    Object? capacity = freezed,
+    Object? color = freezed,
+    Object? id = freezed,
+    Object? images = freezed,
+    Object? isFavorites = freezed,
+    Object? price = freezed,
+    Object? rating = freezed,
+    Object? sd = freezed,
+    Object? ssd = freezed,
+    Object? title = freezed,
+  }) {
+    return _then(_$_DetailsModel(
+      cpu: cpu == freezed
+          ? _value.cpu
+          : cpu // ignore: cast_nullable_to_non_nullable
+              as String,
+      camera: camera == freezed
+          ? _value.camera
+          : camera // ignore: cast_nullable_to_non_nullable
+              as String,
+      capacity: capacity == freezed
+          ? _value._capacity
+          : capacity // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      color: color == freezed
+          ? _value._color
+          : color // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      images: images == freezed
+          ? _value._images
+          : images // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      isFavorites: isFavorites == freezed
+          ? _value.isFavorites
+          : isFavorites // ignore: cast_nullable_to_non_nullable
+              as bool,
+      price: price == freezed
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as int,
+      rating: rating == freezed
+          ? _value.rating
+          : rating // ignore: cast_nullable_to_non_nullable
+              as double,
+      sd: sd == freezed
+          ? _value.sd
+          : sd // ignore: cast_nullable_to_non_nullable
+              as String,
+      ssd: ssd == freezed
+          ? _value.ssd
+          : ssd // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_DetailsModel implements _DetailsModel {
+  const _$_DetailsModel(
+      {@JsonKey(name: 'CPU') required this.cpu,
+      @JsonKey(name: 'camera') required this.camera,
+      @JsonKey(name: 'capacity') required final List<String> capacity,
+      @JsonKey(name: 'color') required final List<String> color,
+      @JsonKey(name: 'id') required this.id,
+      @JsonKey(name: 'images') required final List<String> images,
+      @JsonKey(name: 'isFavorites') required this.isFavorites,
+      @JsonKey(name: 'price') required this.price,
+      @JsonKey(name: 'rating') required this.rating,
+      @JsonKey(name: 'sd') required this.sd,
+      @JsonKey(name: 'ssd') required this.ssd,
+      @JsonKey(name: 'title') required this.title})
+      : _capacity = capacity,
+        _color = color,
+        _images = images;
+
+  factory _$_DetailsModel.fromJson(Map<String, dynamic> json) =>
+      _$$_DetailsModelFromJson(json);
+
+  @override
+  @JsonKey(name: 'CPU')
+  final String cpu;
+  @override
+  @JsonKey(name: 'camera')
+  final String camera;
+  final List<String> _capacity;
+  @override
+  @JsonKey(name: 'capacity')
+  List<String> get capacity {
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_capacity);
+  }
+
+  final List<String> _color;
+  @override
+  @JsonKey(name: 'color')
+  List<String> get color {
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_color);
+  }
+
+  @override
+  @JsonKey(name: 'id')
+  final String id;
+  final List<String> _images;
+  @override
+  @JsonKey(name: 'images')
+  List<String> get images {
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_images);
+  }
+
+  @override
+  @JsonKey(name: 'isFavorites')
+  final bool isFavorites;
+  @override
+  @JsonKey(name: 'price')
+  final int price;
+  @override
+  @JsonKey(name: 'rating')
+  final double rating;
+  @override
+  @JsonKey(name: 'sd')
+  final String sd;
+  @override
+  @JsonKey(name: 'ssd')
+  final String ssd;
+  @override
+  @JsonKey(name: 'title')
+  final String title;
+
+  @override
+  String toString() {
+    return 'DetailsModel(cpu: $cpu, camera: $camera, capacity: $capacity, color: $color, id: $id, images: $images, isFavorites: $isFavorites, price: $price, rating: $rating, sd: $sd, ssd: $ssd, title: $title)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_DetailsModel &&
+            const DeepCollectionEquality().equals(other.cpu, cpu) &&
+            const DeepCollectionEquality().equals(other.camera, camera) &&
+            const DeepCollectionEquality().equals(other._capacity, _capacity) &&
+            const DeepCollectionEquality().equals(other._color, _color) &&
+            const DeepCollectionEquality().equals(other.id, id) &&
+            const DeepCollectionEquality().equals(other._images, _images) &&
+            const DeepCollectionEquality()
+                .equals(other.isFavorites, isFavorites) &&
+            const DeepCollectionEquality().equals(other.price, price) &&
+            const DeepCollectionEquality().equals(other.rating, rating) &&
+            const DeepCollectionEquality().equals(other.sd, sd) &&
+            const DeepCollectionEquality().equals(other.ssd, ssd) &&
+            const DeepCollectionEquality().equals(other.title, title));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(cpu),
+      const DeepCollectionEquality().hash(camera),
+      const DeepCollectionEquality().hash(_capacity),
+      const DeepCollectionEquality().hash(_color),
+      const DeepCollectionEquality().hash(id),
+      const DeepCollectionEquality().hash(_images),
+      const DeepCollectionEquality().hash(isFavorites),
+      const DeepCollectionEquality().hash(price),
+      const DeepCollectionEquality().hash(rating),
+      const DeepCollectionEquality().hash(sd),
+      const DeepCollectionEquality().hash(ssd),
+      const DeepCollectionEquality().hash(title));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_DetailsModelCopyWith<_$_DetailsModel> get copyWith =>
+      __$$_DetailsModelCopyWithImpl<_$_DetailsModel>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_DetailsModelToJson(
+      this,
+    );
+  }
+}
+
+abstract class _DetailsModel implements DetailsModel {
+  const factory _DetailsModel(
+      {@JsonKey(name: 'CPU') required final String cpu,
+      @JsonKey(name: 'camera') required final String camera,
+      @JsonKey(name: 'capacity') required final List<String> capacity,
+      @JsonKey(name: 'color') required final List<String> color,
+      @JsonKey(name: 'id') required final String id,
+      @JsonKey(name: 'images') required final List<String> images,
+      @JsonKey(name: 'isFavorites') required final bool isFavorites,
+      @JsonKey(name: 'price') required final int price,
+      @JsonKey(name: 'rating') required final double rating,
+      @JsonKey(name: 'sd') required final String sd,
+      @JsonKey(name: 'ssd') required final String ssd,
+      @JsonKey(name: 'title') required final String title}) = _$_DetailsModel;
+
+  factory _DetailsModel.fromJson(Map<String, dynamic> json) =
+      _$_DetailsModel.fromJson;
+
+  @override
+  @JsonKey(name: 'CPU')
+  String get cpu;
+  @override
+  @JsonKey(name: 'camera')
+  String get camera;
+  @override
+  @JsonKey(name: 'capacity')
+  List<String> get capacity;
+  @override
+  @JsonKey(name: 'color')
+  List<String> get color;
+  @override
+  @JsonKey(name: 'id')
+  String get id;
+  @override
+  @JsonKey(name: 'images')
+  List<String> get images;
+  @override
+  @JsonKey(name: 'isFavorites')
+  bool get isFavorites;
+  @override
+  @JsonKey(name: 'price')
+  int get price;
+  @override
+  @JsonKey(name: 'rating')
+  double get rating;
+  @override
+  @JsonKey(name: 'sd')
+  String get sd;
+  @override
+  @JsonKey(name: 'ssd')
+  String get ssd;
+  @override
+  @JsonKey(name: 'title')
+  String get title;
+  @override
+  @JsonKey(ignore: true)
+  _$$_DetailsModelCopyWith<_$_DetailsModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
