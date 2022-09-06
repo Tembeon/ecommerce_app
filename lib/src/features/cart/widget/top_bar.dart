ae4a1faa10180e32edca7057440aafc181647d96
Tembeon <tembeon@gmail.com>
2022-09-06 15:59:23 +0300

    separate from one widget to a few one

--- lib/src/features/cart/widget/top_bar.dart
+++ lib/src/features/cart/widget/top_bar.dart
@@ -15,49 +15,82 @@ class ToolBar extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 38.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Material(
-              borderRadius: BorderRadius.circular(10),
-              color: Theme.of(context).colorScheme.primary,
-              child: InkWell(
-                borderRadius: BorderRadius.circular(10),
-                onTap: () => context.navigateBack(),
-                child: SizedBox.square(
-                  dimension: 46,
-                  child: Icon(
-                    Icons.arrow_back_ios_new,
-                    color: Theme.of(context).colorScheme.onPrimary,
-                  ),
-                ),
-              ),
-            ),
-            const Spacer(),
-            Text(
-              S.of(context).addAddress,
-              style: const TextStyle(
-                fontFamily: FontFamily.markPro,
-                fontWeight: FontWeight.w500,
-                fontSize: 18.0,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(width: 12),
-            Material(
-              borderRadius: BorderRadius.circular(10),
-              color: Theme.of(context).colorScheme.secondary,
-              child: InkWell(
-                borderRadius: BorderRadius.circular(10),
-                onTap: () {},
-                child: SizedBox.square(
-                  dimension: 46,
-                  child: Icon(
-                    Icons.shopping_bag_outlined,
-                    color: Theme.of(context).colorScheme.onSecondary,
-                  ),
-                ),
-              ),
-            ),
+          children: const [
+            _ToolBarBackButton(),
+            Spacer(),
+            _ToolBarAddAddressText(),
+            SizedBox(width: 12),
+            _ToolBarAddressButton(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ToolBarAddAddressText extends StatelessWidget {
+  const _ToolBarAddAddressText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      S.of(context).addAddress,
+      style: const TextStyle(
+        fontFamily: FontFamily.markPro,
+        fontWeight: FontWeight.w500,
+        fontSize: 18.0,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class _ToolBarAddressButton extends StatelessWidget {
+  const _ToolBarAddressButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      borderRadius: BorderRadius.circular(10),
+      color: Theme.of(context).colorScheme.secondary,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(10),
+        onTap: () {},
+        child: SizedBox.square(
+          dimension: 46,
+          child: Icon(
+            Icons.shopping_bag_outlined,
+            color: Theme.of(context).colorScheme.onSecondary,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ToolBarBackButton extends StatelessWidget {
+  const _ToolBarBackButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      borderRadius: BorderRadius.circular(10),
+      color: Theme.of(context).colorScheme.primary,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(10),
+        onTap: () => context.navigateBack(),
+        child: SizedBox.square(
+          dimension: 46,
+          child: Icon(
+            Icons.arrow_back_ios_new,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
         ),
       ),
     );
