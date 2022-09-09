cafbcf892a04046ae16fd4d1567d8602985cd100
Tembeon <tembeon@gmail.com>
2022-09-09 13:59:41 +0300

    setup service locator on app startup

--- lib/runner_web.dart
+++ lib/runner_web.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl_browser.dart';
 
+import 'src/core/service_locator/service_locator.dart';
 import 'src/core/widget/app.dart';
 
 /// App running on web (html)
 Future<void> run() async {
   // find system locale based on web window
   await findSystemLocale();
+  setupServiceLocator();
 
   return runApp(const EcommerceApp());
 }
